2d48202a8b3c63bd04451799cd83e8f5ed1c09a8
windiewhale <feketebodza@gmail.com>
2022-03-28 16:20:44 +0200

    working solution from Krisz

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
+const { json } = require("body-parser");
 const express = require("express");
 const fileUpload = require("express-fileupload");
+const { fstat } = require("fs");
 const path = require("path");
 const app = express(); 
 
@@ -12,21 +14,40 @@ app.get("/", getFunction);
 app.use("/upload", express.static(`${__dirname}/../frontend/upload`));
 app.use("/pub", express.static(`${__dirname}/../frontend/public`));
 
-const port = 9000;
-const ipAddress = `http://127.0.0.1:${port}`;
-app.listen(port, () => {
-    console.log(ipAddress)
-});
+const uploads = path.join(`${__dirname}/../frontend/upload`)
+app.post("/", (req, res) => {
+    
+    //upload img
+    const picture = req.files.picture;
+    if (picture){
+        console.dir(picture)
+        picture.mv(uploads + "profile.jpg")  // logolja az adatait, így működik épp.
+    }
+ /* 
+    //upload data 
+    const formData = req.body;
+    formData.image_name = picture.name
+    jsonData.push(formData);
 
-app.post('/upload', function(req, res) {
+   fs.writeFile(`${dataLocation}data.json`, JSON.stringify(jsonData), (error) => {
+        if (error) {
+            console.log(error);
+        }
+    }) */
+})
+
+
+
+
+/* app.post("/", function(req, res) {  //itt stimmelni kell a az enpointnak a script.js fetchnél megadottal
     let sampleFile;
     let uploadPath;
   
     if (!req.files || Object.keys(req.files).length === 0) {
       return res.status(400).send('No files were uploaded.');
     }
-    sampleFile = req.files.sampleFile;
-    uploadPath = __dirname + '../frontend/upload' + sampleFile.name;
+    sampleFile = req.files.picture;
+    uploadPath = __dirname + '../frontend/upload' + picure.name;
 
     sampleFile.mv(uploadPath, function(err) {
         if (err)
@@ -34,4 +55,10 @@ app.post('/upload', function(req, res) {
     
         res.send('File uploaded!');
       });
+}); */
+
+const port = 9000;
+const ipAddress = `http://127.0.0.1:${port}`;
+app.listen(port, () => {
+    console.log(ipAddress)
 });
--- frontend/public/script.js
+++ frontend/public/script.js
@@ -18,16 +18,16 @@ function loadEvent() {
         e.preventDefault();
        /*  console.dir(e); */
 
-        const formData = new FormData() //doboz amiben küldjük az adatokat
-        formData.append("title", e.target.querySelector(`input[name="title"]`).value) // e.target, mert a for esetében az egész formra vonatkozik a submit tehát meg lehet fogni ígyis
-        formData.append("picture", e.target.querySelector(`input[name="picture"]`).files[0]) //akár egy vagy több mindig tömb jön létre, így ha csak egy akkor a 0. elem kell
+        const formData = new FormData() 
+        formData.append("title", e.target.querySelector(`input[name="title"]`).value) 
+        formData.append("picture", e.target.querySelector(`input[name="picture"]`).files[0]) 
 
         const fetchSettings = {
             method: "POST",
             body: formData
         }
 
-        fetch("/", fetchSettings) //megadjuk az url ahova postolnánk ez "/", aztán egy objektumot amit elmentettünk változóba
+        fetch("/", fetchSettings) 
             .then(data => {
                 if (data.status === 200) {
                     e.target.outerHTML = "done"
@@ -37,7 +37,7 @@ function loadEvent() {
             .catch(error => {
                 e.target.outerHTML = "error";
                 console.dir(error)
-            } )
+            })
     })
 }
 
